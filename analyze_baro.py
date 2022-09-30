5bc73466d3318bd1604ed890dcb568989d385cbe
hakasapl <hakansaplakog@gmail.com>
2022-09-29 23:24:15 -0400

    added event log option

--- analyze_baro.py
+++ analyze_baro.py
@@ -11,23 +11,40 @@ from scipy.signal import butter, lfilter, detrend, welch, spectrogram
 from scipy.signal.windows import hamming
 import numpy as np
 from obspy.signal.util import next_pow_2
+import csv
 
 def main():
     now = datetime.datetime.now()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("data", help="Location of data output", nargs=1, type=str, default="data")
-    parser.add_argument("name", help="Name of event being analyzed", nargs=1, type=str)
-    parser.add_argument("-s", "--start-time", help="Location of output data", type=str, default="1970-01-01-00-00-00")
-    parser.add_argument("-e", "--end-time", help="Period, in ms, between each data push", type=str, default=now.strftime("%Y-%m-%d-%H-%M-%S"))
-    parser.add_argument("-l", "--line", help="Point to mark vertical line on charts", action='append')
+    parser.add_argument("-n", "--name", help="Name of event being analyzed", nargs=1, type=str)
+    parser.add_argument("-f", "--event-log", help="Path to event log", type=str, default="")
+    parser.add_argument("-s", "--start-time", help="Start time of window", type=str, default="1970-01-01-00-00-00")
+    parser.add_argument("-e", "--end-time", help="End time of window", type=str, default=now.strftime("%Y-%m-%d-%H-%M-%S"))
+    parser.add_argument("-l", "--line", help="Timestamp to mark vertical line on charts", action='append')
     parser.add_argument("-p", "--show-plots", help="Show interactive plots", action="store_true")
     args = parser.parse_args()
     print(args)
 
-    start_time = datetime.datetime.strptime(args.start_time, '%Y-%m-%d-%H-%M-%S')
-    end_time = datetime.datetime.strptime(args.end_time, '%Y-%m-%d-%H-%M-%S')
-    event_name = args.name[0]
+    if args.event_log == "":
+        genGraphs(args.name[0], args.data[0], args.start_time, args.end_time, args.line, args.show_plots)
+    else:
+        # event log available
+        with open(args.event_log, 'r') as csvfile:
+            datareader = csv.reader(csvfile)
+            for row in datareader:
+                print(row)
+
+                lines = row[1].split("|")
+
+                genGraphs(row[0], args.data[0], row[2], row[3], lines, args.show_plots)
+
+                
+def genGraphs(str_eventname, str_datapath, str_starttime, str_endtime, str_events, show_plots = False):
+    start_time = datetime.datetime.strptime(str_starttime, '%Y-%m-%d-%H-%M-%S')
+    end_time = datetime.datetime.strptime(str_endtime, '%Y-%m-%d-%H-%M-%S')
+    event_name = str_eventname
     output_loc = "output/" + event_name
     if not os.path.exists(output_loc):
         os.mkdir(output_loc)
@@ -36,7 +53,7 @@ def main():
         print("End time must be after start time!")
         exit(1)
 
-    df = pd.read_csv(args.data[0])
+    df = pd.read_csv(str_datapath)
     df['timestamp'] = pd.to_datetime(df['timestamp'])  # change this if timestamp format every changes in the data
     
     # filter out data outside of time range
@@ -91,14 +108,14 @@ def main():
     plt.ylabel("Pressure (hPa)", fontsize=18, loc='center')
 
     # plot event lines
-    for event in args.line:
+    for event in str_events:
         event_dt = datetime.datetime.strptime(event, '%Y-%m-%d-%H-%M-%S')
         for ax in axes:
             ax.axvline(event_dt, color='k', linestyle='--')
 
     plt.savefig(output_loc + "/baro.png")
 
-    if args.show_plots:
+    if show_plots:
         plt.show()
 
     #
@@ -108,12 +125,12 @@ def main():
     for device in new_cols:
         df[device] *= 100  # convert to Pa from hPa
 
-        welchB = 600
-        welch0 = 100
-        NFFT = 2^next_pow_2(welchB)
+        welchB = 256
+        welch0 = 32
         w_window = hamming(welchB)
 
-        f, t, Sxx = spectrogram(df[device], fs=20, window=w_window, noverlap=welch0, nfft=NFFT, return_onesided=True, mode='psd')
+        #f, t, Sxx = spectrogram(df[device], fs=20, window=w_window, noverlap=welch0, nfft=NFFT, return_onesided=True, mode='psd')
+        f, t, Sxx = spectrogram(df[device], fs=20, window=w_window, noverlap=welch0, nfft=welchB, return_onesided=True, mode='psd')
         
         # convert to timestamps (from seconds)
         t_timestamps = []
@@ -127,14 +144,16 @@ def main():
         plt.ylabel('Frequency (Hz)', fontsize=18)
         plt.xlabel('Timestamp (UTC)', fontsize=18)
         plt.colorbar(label='Pa^2/Hz')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
 
-        for event in args.line:
+        for event in str_events:
             event_dt = datetime.datetime.strptime(event, '%Y-%m-%d-%H-%M-%S')
             plt.axvline(event_dt, color='k', linestyle='--')
         
         plt.savefig(output_loc + "/" + device + "-spec.png")
 
-        if args.show_plots:
+        if show_plots:
             plt.show()
 
 if __name__ == "__main__":
--- analyze_wind.py
+++ analyze_wind.py
@@ -11,13 +11,15 @@ from scipy.signal import butter, lfilter, detrend, welch, spectrogram
 from scipy.signal.windows import hamming
 import numpy as np
 from obspy.signal.util import next_pow_2
+import csv
 
 def main():
     now = datetime.datetime.now()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("data", help="Location of data output", nargs=1, type=str, default="data")
-    parser.add_argument("name", help="Name of event being analyzed", nargs=1, type=str)
+    parser.add_argument("-n", "--name", help="Name of event being analyzed", nargs=1, type=str)
+    parser.add_argument("-f", "--event-log", help="Path to event log", type=str, default="")
     parser.add_argument("-s", "--start-time", help="Location of output data", type=str, default="1970-01-01-00-00-00")
     parser.add_argument("-e", "--end-time", help="Period, in ms, between each data push", type=str, default=now.strftime("%Y-%m-%d-%H-%M-%S"))
     parser.add_argument("-l", "--line", help="Point to mark vertical line on charts", action='append')
@@ -25,9 +27,23 @@ def main():
     args = parser.parse_args()
     print(args)
 
-    start_time = datetime.datetime.strptime(args.start_time, '%Y-%m-%d-%H-%M-%S')
-    end_time = datetime.datetime.strptime(args.end_time, '%Y-%m-%d-%H-%M-%S')
-    event_name = args.name[0]
+    if args.event_log == "":
+        genGraphs(args.name[0], args.data[0], args.start_time, args.end_time, args.line, args.show_plots)
+    else:
+        # event log available
+        with open(args.event_log, 'r') as csvfile:
+            datareader = csv.reader(csvfile)
+            for row in datareader:
+                print(row)
+
+                lines = row[1].split("|")
+
+                genGraphs(row[0], args.data[0], row[2], row[3], lines, args.show_plots)
+
+def genGraphs(str_eventname, str_datapath, str_starttime, str_endtime, str_events, show_plots = False):
+    start_time = datetime.datetime.strptime(str_starttime, '%Y-%m-%d-%H-%M-%S')
+    end_time = datetime.datetime.strptime(str_endtime, '%Y-%m-%d-%H-%M-%S')
+    event_name = str_eventname
     output_loc = "output/" + event_name
     if not os.path.exists(output_loc):
         os.mkdir(output_loc)
@@ -36,7 +52,7 @@ def main():
         print("End time must be after start time!")
         exit(1)
 
-    df = pd.read_csv(args.data[0])
+    df = pd.read_csv(str_datapath)
     df['timestamp'] = pd.to_datetime(df['timestamp'])  # change this if timestamp format every changes in the data
     
     # filter out data outside of time range
@@ -78,14 +94,14 @@ def main():
     plt.ylabel("Speed (m/s)", fontsize=18, loc='center')
 
     # plot event lines
-    for event in args.line:
+    for event in str_events:
         event_dt = datetime.datetime.strptime(event, '%Y-%m-%d-%H-%M-%S')
         for ax in axes:
             ax.axvline(event_dt, color='k', linestyle='--')
 
     plt.savefig(output_loc + "/wind.png")
 
-    if args.show_plots:
+    if show_plots:
         plt.show()
 
 if __name__ == "__main__":
